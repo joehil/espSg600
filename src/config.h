3bf24bbfc757a42ec5fe6dc10854da50489fcdcc
Joerg Hillebrand <hillebrand@jhillebrand.de>
2023-07-30 16:49:18 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,27 @@
+// WiFi AP SSID
+#define WIFI_SSID "<ssid>"
+// WiFi password
+#define WIFI_PASSWORD "<password>"
+
+#define SET_PIN 5
+#define CS_PIN 13
+
+#define RXD2 16
+#define TXD2 17
+
+//LC12S Parameters
+#define BAUD 0x04
+#define MODULE_ID 0x2233
+#define NETWORK_ID 0x1122
+#define RF_TRANSMIT_POWER 0x00
+#define RF_CHANNEL 0x64
+
+//Inverter Parameters
+#define BOX_ID 0x98, 0x76 //Box ID
+#define INVERTER_ID 0x38, 0x00, 0x66, 0x22
+
+#define Inverter_id "38006622";
+#define Box_id "9876";
+
+#define I_BOX_ID 2
+#define I_INVERTER_ID 6
